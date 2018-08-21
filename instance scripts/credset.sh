7a07032c2bf6ed476721c6eecc588d668e57be53
Sumit Kumar <skuma115@asu.edu>
2018-08-20 20:15:04 -0700

    Update credset.sh

--- instance scripts/credset.sh
+++ instance scripts/credset.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-aws configure set aws_access_key_id AKIAJMNZAIPO2LP6EVPA
-aws configure set aws_secret_access_key 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
+aws configure set aws_access_key_id 
+aws configure set aws_secret_access_key 
 aws configure set default.region us-west-2
 aws configure set default.output text
 echo "credential set"
